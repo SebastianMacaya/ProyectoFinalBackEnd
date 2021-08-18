a457d1116f399468238ff613c134fb277456ec88
SebastianMacaya <contacto@sebastianmacaya.com>
2021-08-18 15:52:50 -0300

    upload desafio04

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
-const routes = require("./src/routes/app");
+const routes = require("./src/routes/routes");
+
+const PORT = 8080;
+
+//middleware
 const app = express();
-app.use(express.static("public"));
-
-const PORT = 3000;
-
-//middlewares
-
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//Routes
 app.use(routes);
+
 app.use("/static", express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log("Server on localhost:" + PORT);
